6f2da4dcda278cbacf6100fa1cb75c7d7220fd33
akotagi23 <akotagi23@gmail.com>
2025-12-04 18:48:43 -0500

    Update edge_detect.h

--- hls/edge_detection/edge_detect.h
+++ hls/edge_detection/edge_detect.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <ap_int.h>
 
 #define MAX_WIDTH 960
@@ -7,32 +6,12 @@
 
 typedef ap_uint<8> pix_t;
 
-// Gaussian blur kernel
-static const int GAUSS[3][3] = {
-    {1, 2, 1},
-    {2, 4, 2},
-    {1, 2, 1}
-};
-
-// Sobel X kernel
-static const int SOBEL_X[3][3] = {
-    {-1, 0, 1},
-    {-2, 0, 2},
-    {-1, 0, 1}
-};
-
-// Sobel Y kernel
-static const int SOBEL_Y[3][3] = {
-    {-1, -2, -1},
-    { 0,  0,  0},
-    { 1,  2,  1}
-};
-
 void edge_detect(
-    pix_t in_img[MAX_HEIGHT][MAX_WIDTH],
-    pix_t out_img[MAX_HEIGHT][MAX_WIDTH],
+    unsigned char *in_img,
+    unsigned char *out_img,
     int rows,
     int cols,
     int low_thresh,
     int high_thresh
 );
+
