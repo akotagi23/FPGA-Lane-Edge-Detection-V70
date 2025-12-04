fa59a4dcd63739df46017c4d8c1517edaaf35aad
akotagi23 <akotagi23@gmail.com>
2025-12-04 18:50:43 -0500

    Update script.tcl

--- hls/edge_detection/script.tcl
+++ hls/edge_detection/script.tcl
@@ -3,17 +3,23 @@ set_top edge_detect
 
 add_files edge_detect.cpp
 add_files edge_detect.h
+#add_files -tb tb_edge.cpp
 
 open_solution -reset "solution1"
-
 set_part {xcu280-fsvh2892-2L-e}
-create_clock -period 3.33
+create_clock -period 5
 
-# IMPORTANT FIX — configure export BEFORE synthesis
+# MUST COME BEFORE CSYNTH
 config_export -format xo
 
+# Simulation
+#csim_design
+
+# Synthesis
 csynth_design
 
+# Export
 export_design -format xo -output edge_detect.xo
+
 exit
 
